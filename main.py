db93e81681ee398d2a814ca648a0727fdd8b9e18
Corey Hastings <stingzld@gmail.com>
2025-05-25 16:41:44 -0500

    Updated and reorganized main() code

--- main.py
+++ main.py
@@ -15,14 +15,13 @@ def main():
             if event.type == pygame.QUIT:  # if user clicked on window's close button
                 return
 
-        pygame.Surface.fill(screen, "black")
-
         player.update(dt)
-        player.draw(screen)
 
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000  # limits framerate to 60 FPS
 
 
 if __name__ == "__main__":

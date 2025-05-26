dae4be2300675675d0e3344d6bd6b4477ae8be27
Corey Hastings <stingzld@gmail.com>
2025-05-25 21:00:50 -0500

    Added collision detection

--- main.py
+++ main.py
@@ -7,21 +7,20 @@ from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+
     Player.containers = updatable, drawable
     Asteroid.containers = updatable, drawable, asteroids
     AsteroidField.containers = updatable
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     asteroid_field = AsteroidField()
-
-    clock = pygame.time.Clock()
-    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -30,9 +29,16 @@ def main():
 
         updatable.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                exit()
+
         screen.fill("black")
-        for item in drawable:
-            item.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # limits framerate to 60 FPS

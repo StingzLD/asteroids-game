cb4c63694f4c517985fb765363d6116d50d581e6
Corey Hastings <stingzld@gmail.com>
2025-05-25 20:25:16 -0500

    Created groups | Modified main() update and draw code

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from player import Player
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -15,10 +21,11 @@ def main():
             if event.type == pygame.QUIT:  # if user clicked on window's close button
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # limits framerate to 60 FPS
